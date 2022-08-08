bfaa5c6c1b24b3286a6897dc9e186aa70be637ce
Batista Harahap <batista@bango29.com>
2022-08-09 00:05:40 +0700

    migrate to poetry for deps

---
+++ goingfast/traders/binancefutures.py
@@ -0,0 +1,15 @@
+from goingfast import BaseTrader
+
+
+class BinanceFutures(BaseTrader):
+    async def long_entry(self):
+        pass
+
+    async def long_exit(self):
+        pass
+
+    async def short_entry(self):
+        pass
+
+    async def short_exit(self):
+        pass
